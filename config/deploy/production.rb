1e2086647996ecd52dd87de6826035b78e21bc9b
Syed Ishtiaque Ahmad <ishtiaque05@gmail.com>
2016-11-01 11:26:56 +0600

    capistrano deploy file written

--- config/deploy/production.rb
+++ config/deploy/production.rb
@@ -1,3 +1,6 @@
+set :stage, :production
+server '192.168.1.92', user: 'deploy', roles: %w{web app db}
+
 # server-based syntax
 # ======================
 # Defines a single server with a list of roles and multiple properties.
